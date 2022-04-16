c8257b969ace18d871848c1631bf4a05dd9db42f
Eun Jin Kim <eunjinkim@macbook-pro.myfiosgateway.com>
2022-04-16 18:01:57 -0400

    update

--- main.js
+++ main.js
@@ -48,13 +48,15 @@ fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
             <div>
                 <a href="${data.url}">${data.title}</a>
                 <div>
-                    ${data.score} points by ${data.by} 
+                    ${data.score} points by ${data.by}      ||  ${data.kids.length} comments
                 </div>
             </div>
             `
 
             child.appendChild(story);
             storyList.appendChild(child);
+
+            
 
                     
         })
@@ -63,3 +65,14 @@ fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
 
 })
 
+// fetch("https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty")
+// .then(function(httpResponse){
+//     return httpResponse.json();
+// })
+// .then(function(data){
+//     console.log(data.kids.length);
+// })
+
+// let test = [ 8952, 9224, 8917, 8884, 8887, 8943, 8869, 8958, 9005, 9671, 8940, 9067, 8908, 9055, 8865, 8881, 8872, 8873, 8955, 10403, 8903, 8928, 9125, 8998, 8901, 8902, 8907, 8894, 8878, 8870, 8980, 8934, 8876 ];
+
+// console.log(test.length)
