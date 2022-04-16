26d44a53e011b6a7b9c627d841a8c4c72e6191b6
Eun Jin Kim <eunjinkim@macbook-pro.myfiosgateway.com>
2022-04-16 12:44:52 -0400

    upload

---
+++ main.js
@@ -0,0 +1,51 @@
+const body = document.querySelector('body');
+
+const parent = document.createElement('div');
+parent.className = 'parent';
+
+//parent.appendChild(child);
+body.appendChild(parent);
+
+// let test = document.createTextNode("Test text");
+// child.appendChild(test);
+
+//=========================================================================
+//Get top 100 IDs:
+
+fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+    
+.then(function(httpRequest){
+        return httpRequest.json();
+    })
+    
+.then(function(data){
+    let arr = data.slice(0, 99);
+    //console.log(arr);
+
+    //loop through arr to grag each story number:
+        for (let i = 0; i < arr.length; i++){
+            let storyID = arr[i]
+
+            //put story number in api format
+            fetch(`https:hacker-news.firebaseio.com/v0/item/${storyID}.json?print=pretty`)
+            
+            .then(function(httpReponse){
+                return httpReponse.json();
+            })
+
+            .then(function(data){
+
+                let child = document.createElement('div');
+                child.className = 'child';
+
+                let story = document.createElement("li");
+                story.innerText = data.title;
+
+                child.appendChild(story);
+                parent.appendChild(child);
+                
+            })
+
+            }
+        })
+
